a2f3114204db81789df03b2390b3e660573fa59e
r9988.2588 <r9988.2588@gmail.com>
2024-11-19 09:58:55 +0000

    added the main screens with all important packages installed also added dev.nix

--- App.tsx
+++ App.tsx
@@ -1,24 +1,39 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { NavigationContainer, createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// screens
+import Home from './screens/Home';
+import Shorts from './screens/Shorts';
+import Subscriptions from './screens/Subscriptions';
+import Plus from './screens/Plus';
+import You from './screens/You';
+
+const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+const Main = () => {
+return (
+  <BottomTab.Navigator>
+    <BottomTab.Screen name="Home" component={Home} />
+    <BottomTab.Screen name="Shorts" component={Shorts} />
+    <BottomTab.Screen name="Plus" component={Plus} />
+    <BottomTab.Screen name="Subscriptions" component={Subscriptions} />
+    <BottomTab.Screen name="You" component={You} />
+  </BottomTab.Navigator>
+)
+}
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Youtube Clone</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={Main} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 30,
-    fontWeight: 'bold',
-  }
-})
