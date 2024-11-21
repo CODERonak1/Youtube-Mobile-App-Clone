cf61de8a0570a545f49b06fa82030c90c21d3918
r9988.2588 <r9988.2588@gmail.com>
2024-11-21 09:30:31 +0000

    added status bar and done few changes

--- App.tsx
+++ App.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 import { NavigationContainer, createStaticNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar } from 'expo-status-bar';
 
 // icons
-import Foundation from '@expo/vector-icons/Foundation';
+import Entypo from '@expo/vector-icons/Entypo';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
@@ -20,53 +21,62 @@ const Stack = createNativeStackNavigator();
 const BottomTab = createBottomTabNavigator();
 
 const Main = () => {
-return (
-  <BottomTab.Navigator
-  screenOptions={{
-    tabBarActiveTintColor: 'black',
-    tabBarInactiveTintColor: 'gray',
-    tabBarShowLabel: true
-    
-  }}
-  >
-    <BottomTab.Screen name="Home" component={Home} options={{
-      tabBarIcon: ({color, size}) => (
-        <Foundation name="home" size={size} color={color} />
-      )
-    }}/>
-
-    <BottomTab.Screen name="Shorts" component={Shorts} options={{
-      tabBarIcon: ({color, size}) => (
-        <AntDesign name="youtube" size={size} color={color} />
-      )
-    }}/>
-    <BottomTab.Screen name="Plus" component={Plus} options={{
-      tabBarIcon: ({color, size}) => (
-        <AntDesign name="pluscircleo" size={size} color={color} />
-      ),
-      tabBarShowLabel: false,
+  return (
+    <>
+      <StatusBar style='dark' backgroundColor='#00000020' />
       
+      <BottomTab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'gray',
+          tabBarShowLabel: true,
+          tabBarStyle: {
+            height: 60,
+          },
+          tabBarLabelStyle: {
+            color: 'black'
+          }
+        }}
+      >
+        <BottomTab.Screen name="Home" component={Home} options={{
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="home" size={size} color={color} />
+          ),
+          headerShown: false
+        }} />
 
-    }}/>
-    <BottomTab.Screen name="Subscriptions" component={Subscriptions} options={{
-      tabBarIcon: ({color, size}) => (
-        <MaterialIcons name="subscriptions" size={size} color={color} />
-      )
-    }}/>
-    <BottomTab.Screen name="You" component={You} options={{
-      tabBarIcon: ({color, size}) => (
-        <MaterialIcons name="account-circle" size={size} color={color} />
-      )
-    }}/>
-  </BottomTab.Navigator>
-)
+        <BottomTab.Screen name="Shorts" component={Shorts} options={{
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="youtube" size={size} color={color} />
+          )
+        }} />
+        <BottomTab.Screen name="Plus" component={Plus} options={{
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="plus" size={size} color={color} />
+          ),
+
+          tabBarLabel: '',
+        }} />
+        <BottomTab.Screen name="Subscriptions" component={Subscriptions} options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="subscriptions" size={size} color={color} />
+          )
+        }} />
+        <BottomTab.Screen name="You" component={You} options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="account-circle" size={size} color={color} />
+          )
+        }} />
+      </BottomTab.Navigator></>
+    // </SafeAreaView>
+  )
 }
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Main" component={Main} options={{headerShown: false}}/>
+        <Stack.Screen name="Main" component={Main} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   )
